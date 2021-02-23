4c6d0b0be837b2f385c9b6d740732cf502be2ff8
Maciej Szulia <maciej.szulia@gmail.com>
2021-02-23 17:03:26 +0100

    start

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ print("your name is " + name)
 #age = int(age)
 
 age = int(input("Enter your age: "))
+
+print("Were" + "wolf")
+print("Door" + "man")
+print("4" + "chan")
+print(str(4) + "chan")
+
+print(4 * "test ")
