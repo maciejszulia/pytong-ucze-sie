3f45382d128432edb534bb03e091922d2d26834c
Maciej Szulia <maciej.szulia@gmail.com>
2021-02-23 17:45:11 +0100

    lists and for's

---
+++ p4.py
@@ -0,0 +1,14 @@
+import random
+from random_word import RandomWords
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, "Maciek", [0, 1, 2, 3, 4, 5]]
+
+
+
+
+x = 0
+for x in range(100):
+    my_list.append(random.randrange(100))
+
+for element in my_list:
+    print(element)
