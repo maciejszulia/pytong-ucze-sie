645780d0aaa6422e10fc9ff25fb417b11f021f75
Maciej Szulia <maciej.szulia@gmail.com>
2021-02-23 17:36:25 +0100

    lists and random training

--- p3.py
+++ p3.py
@@ -2,11 +2,11 @@ import random
 
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
-i = 0
 
-for x in range(len(a)):
-    if (a[x]) % 2:
-        print(a[x])
+i = 0
+for i in range(len(a)):
+    if (a[i]) % 2:
+        print(a[i])
 
 
 def jezeli_prawda(a, b):
@@ -20,4 +20,4 @@ b = []
 xx = 1
 for xx in range(100):
     b.append(random.randrange(1, 100))
-    print(xx, "=", b[xx], jezeli_prawda(xx, b[xx]))
+    print(xx, "=", b[xx])
