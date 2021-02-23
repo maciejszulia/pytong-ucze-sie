d13aeca006362c5952ed8f5505cd4cbf60e686ef
Maciej Szulia <maciej.szulia@gmail.com>
2021-02-23 17:00:06 +0100

    start

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 name = input("give me your name: ")
 print("your name is " + name)
 
-age = input("enter your age: ")
-age = int(age)
+#age = input("enter your age: ")
+#age = int(age)
+
+age = int(input("Enter your age: "))
