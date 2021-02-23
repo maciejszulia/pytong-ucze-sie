ea031515bd6f441d855ef6c267ffbea32df3fd53
Maciej Szulia <maciej.szulia@gmail.com>
2021-02-23 17:15:13 +0100

    if

--- p2.py
+++ p2.py
@@ -1,9 +1,13 @@
 import random
 
-x = random.randrange(0,10,1)
-y = random.randrange(1,10,2)
+x = random.randrange(0, 10, 1)
+y = random.randrange(1, 10, 2)
+
 print(x)
-if(x) % y:
+if x % y:
     print("true")
 else:
     print("false")
+
+if x == y:
+    print("x=y")
